e386e7821db7950d92762f3ffe084ff4aa111279
seungpang <obey1342@gmail.com>
2019-07-22 21:22:52 +0900

    DRF 맛보기

--- django_/sp_django/product/views.py
+++ django_/sp_django/product/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
+from rest_framework import generics
+from rest_framework import mixins
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterFrom
-from spuser.decorators import login_required
+from .serializers import ProductSerializer
+from spuser.decorators import admin_required
+
+
+class ProductListAPI(generics.GenericAPIView, mixins.ListModelMixin):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all().order_by('id')
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+class ProductDetailAPI(generics.GenericAPIView, mixins.RetrieveModelMixin):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all().order_by('id')
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
 
 class ProductList(ListView):
     model = Product

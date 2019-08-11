35ea01e207f2fcb6ee195e172833e5560941a1b5
seungpang <obey1342@gmail.com>
2019-08-11 16:40:29 +0900

    mailchimp 오류해결

--- blog/src/marketing/views.py
+++ blog/src/marketing/views.py
@@ -12,8 +12,11 @@ MAILCHIMP_API_KEY = settings.MAILCHIMP_API_KEY
 MAILCHIMP_DATA_CENTER= settings.MAILCHIMP_DATA_CENTER
 MAILCHIMP_EMAIL_AUDIENCE_ID = settings.MAILCHIMP_EMAIL_AUDIENCE_ID
 
-api_url = f'https://{MAILCHIMP_DATA_CENTER}.api.mailchimp.com/3.0'
-members_endpoint = f'{api_url}/lists/{MAILCHIMP_EMAIL_AUDIENCE_ID}/members'
+api_url = 'https://{dc}.api.mailchimp.com/3.0'.format(dc=MAILCHIMP_DATA_CENTER)
+members_endpoint = '{api_url}/lists/{list_id}/members'.format(
+    api_url=api_url,
+    list_id=MAILCHIMP_EMAIL_AUDIENCE_ID
+)
 
 def subscribe(email):
     data = {
